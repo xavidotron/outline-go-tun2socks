99677252e44772fbc647720882e36049e00d0239
alalamav <alalamav@users.noreply.github.com>
2019-02-06 16:09:57 -0500

    Remove v2ray and ss handlers, add SOCKS handler

--- tun2socks.go
+++ tun2socks.go
@@ -1,18 +1,12 @@
 package tun2socks
 
 import (
-	"context"
-	"log"
-	"strings"
 	"time"
 
-	vcore "v2ray.com/core"
-	vproxyman "v2ray.com/core/app/proxyman"
-	vbytespool "v2ray.com/core/common/bytespool"
-
 	"github.com/eycorsican/go-tun2socks/core"
-	"github.com/eycorsican/go-tun2socks/proxy/shadowsocks"
-	"github.com/eycorsican/go-tun2socks/proxy/v2ray"
+
+	// "github.com/eycorsican/go-tun2socks/proxy/shadowsocks"
+	"github.com/eycorsican/go-tun2socks/proxy/socks"
 )
 
 type PacketFlow interface {
@@ -25,27 +19,11 @@ func InputPacket(data []byte) {
 	lwipStack.Write(data)
 }
 
-func StartV2Ray(packetFlow PacketFlow, configBytes []byte) {
+func StartSocks(packetFlow PacketFlow, proxyHost string, proxyPort int) {
 	if packetFlow != nil {
 		lwipStack = core.NewLWIPStack()
-
-		core.SetBufferPool(vbytespool.GetPool(core.BufSize))
-
-		v, err := vcore.StartInstance("json", configBytes)
-		if err != nil {
-			log.Fatal("start V instance failed: %v", err)
-		}
-
-		sniffingConfig := &vproxyman.SniffingConfig{
-			Enabled:             true,
-			DestinationOverride: strings.Split("tls,http", ","),
-		}
-		ctx := vproxyman.ContextWithSniffingConfig(context.Background(), sniffingConfig)
-
-		vhandler := v2ray.NewHandler(ctx, v)
-		core.RegisterTCPConnectionHandler(vhandler)
-		core.RegisterUDPConnectionHandler(vhandler)
-
+		core.RegisterTCPConnectionHandler(socks.NewTCPHandler(proxyHost, uint16(proxyPort)))
+		core.RegisterUDPConnectionHandler(socks.NewUDPHandler(proxyHost, uint16(proxyPort), 30*time.Second))
 		core.RegisterOutputFn(func(data []byte) (int, error) {
 			packetFlow.WritePacket(data)
 			return len(data), nil
@@ -53,14 +31,14 @@ func StartV2Ray(packetFlow PacketFlow, configBytes []byte) {
 	}
 }
 
-func StartShadowsocks(packetFlow PacketFlow, proxyHost string, proxyPort int, proxyCipher, proxyPassword string) {
-	if packetFlow != nil {
-		lwipStack = core.NewLWIPStack()
-		core.RegisterTCPConnectionHandler(shadowsocks.NewTCPHandler(core.ParseTCPAddr(proxyHost, uint16(proxyPort)).String(), proxyCipher, proxyPassword))
-		core.RegisterUDPConnectionHandler(shadowsocks.NewUDPHandler(core.ParseUDPAddr(proxyHost, uint16(proxyPort)).String(), proxyCipher, proxyPassword, 30*time.Second))
-		core.RegisterOutputFn(func(data []byte) (int, error) {
-			packetFlow.WritePacket(data)
-			return len(data), nil
-		})
-	}
-}
+// func StartShadowsocks(packetFlow PacketFlow, proxyHost string, proxyPort int, proxyCipher, proxyPassword string) {
+// 	if packetFlow != nil {
+// 		lwipStack = core.NewLWIPStack()
+// 		core.RegisterTCPConnectionHandler(shadowsocks.NewTCPHandler(core.ParseTCPAddr(proxyHost, uint16(proxyPort)).String(), proxyCipher, proxyPassword))
+// 		core.RegisterUDPConnectionHandler(shadowsocks.NewUDPHandler(core.ParseUDPAddr(proxyHost, uint16(proxyPort)).String(), proxyCipher, proxyPassword, 30*time.Second))
+// 		core.RegisterOutputFn(func(data []byte) (int, error) {
+// 			packetFlow.WritePacket(data)
+// 			return len(data), nil
+// 		})
+// 	}
+// }
